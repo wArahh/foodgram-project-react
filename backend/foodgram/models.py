905b2d8cf3f73c7e6296cd2b307b43f4b21b7d13
wArahh <nikitkamakarenko@gmail.com>
2024-04-04 18:01:01 +0300

    base models1 completed

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -9,9 +9,11 @@ class User(AbstractUser):
 class Ingredient(models.Model):
     name = models.CharField(
         max_length=200,
+        verbose_name='Название ингридиента'
     )
     measurement_unit = models.CharField(
         max_length=25,
+        verbose_name='Единица Измерения'
     )
 
     def __str__(self):
@@ -21,35 +23,51 @@ class Ingredient(models.Model):
 class Tag(models.Model):
     name = models.CharField(
         max_length=25,
+        verbose_name='Название тега'
     )
     color_code = models.CharField(
         max_length=7,
+        verbose_name='Цвет'
     )
     slug = models.SlugField(
         max_length=25,
         unique=True,
+        verbose_name='Слаг'
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор'
     )
     name = models.CharField(
         max_length=200,
+        verbose_name='Название рецепта'
     )
     image = models.ImageField(
         upload_to='foodgram/images/',
+        verbose_name='Картинка'
     )
     text = models.TextField(
-        max_length=256
+        max_length=256,
+        verbose_name='текст'
     )
     ingredients = models.ManyToManyField(
-        'Ingredient', related_name='recipes'
+        'Ingredient',
+        related_name='recipes',
     )
     tags = models.ManyToManyField(
-        'Tag', related_name='recipes'
+        'Tag',
+        related_name='recipes'
     )
     cooking_time = models.IntegerField(
+        verbose_name='Время приготовления'
     )
+
+    def __str__(self):
+        return self.name
