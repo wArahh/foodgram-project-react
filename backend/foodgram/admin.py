1e5619b5a2918e336296b012095be455feb010d2
wArahh <nikitkamakarenko@gmail.com>
2024-05-01 16:53:40 +0300

    edited admin

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,23 +1,14 @@
 from django.contrib import admin
 
-from .models import Follow, Ingredient, Recipe, Tag
+from .models import (FavoriteRecipe, Follow, Ingredient,
+                     IngredientAmountForRecipe, Recipe, RecipeShoppingCart,
+                     Tag, User)
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    pass
+@admin.register(
+    User, Tag, Ingredient,
+    Recipe, IngredientAmountForRecipe,
+    FavoriteRecipe, RecipeShoppingCart, Follow
+)
+class AllModelsAdmin(admin.ModelAdmin):
+    list_display = '__all__'
